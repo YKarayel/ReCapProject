b53bf8aca20fe25fca78d050e67890403e23268f
ykarayel <karayelyahya@gmail.com>
2023-10-11 13:17:44 +0300

    Add project files.

---
+++ Business/Concreate/CarManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concreate
+{
+    public class CarManager : ICarManager
+    {
+        
+        private readonly ICarDal _productDal;
+
+        public CarManager(ICarDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Car car)
+        {
+            Console.WriteLine("Car eklendi");
+            _productDal.Add(car);
+        }
+
+        public void Delete(int id)
+        {
+            Console.WriteLine(id + " numaralı data silindi.");
+            _productDal.Delete(id);
+        }
+
+        public List<Car> GetAll()
+        {
+            var data = _productDal.GetAll();
+            Console.WriteLine(data + " GetAll");
+            return data;
+        }
+
+        public string GetById(int id)
+        {
+            var hasData = _productDal.GetById(id).ToString();
+            Console.WriteLine(hasData + "GetById");
+            return _productDal.GetById(id);
+        }
+
+        public void Update(Car car)
+        {
+            Console.WriteLine("Car updatelendi.");
+            _productDal.Update(car);
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concreate;
+using DataAccess.Abstract;
+using DataAccess.Concreate;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+carManager.Add(new Entities.Concreate.Car
+{
+    Id = 6,
+    BrandId = 1,
+    ColorId = 3,
+    DailyPrice = 125000,
+    ModelYear= "2020",
+    Description= "Renault"
+});
+
+carManager.Delete(6);
+carManager.GetAll();
+carManager.Update(new Entities.Concreate.Car
+{
+    Id = 4,
+    BrandId = 4,
+    ColorId = 3,
+    DailyPrice = 299999,
+    Description = "Audi",
+    ModelYear = "2020"
+});
+
+carManager.GetById(4);
